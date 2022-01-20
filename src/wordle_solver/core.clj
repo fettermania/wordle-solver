3419a598a059198ab27da01bb612f935449d8734
Dave Fetterman <fettermania@gmail.com>
2022-01-19 19:41:08 -0800

    First working version

--- src/wordle_solver/core.clj
+++ src/wordle_solver/core.clj
@@ -12,114 +12,110 @@
   (println "Hello, World!"))
 
 
+;; Answers: w_a = 2315
+;; Guesses: w_g = 10657
+
+;; LOAD 
 (def answers-filename "wordle-answers.txt")
 (def allowed-guesses-filename "wordle-allowed-guesses.txt")
 
-(def answers-dict (str/split (slurp answers-filename) #"\n"))
-(def allowed-guesses-dict (str/split (slurp allowed-guesses-filename) #"\n"))
-
-;; Note: doesn't check lengths
-(defn match? [mask word]
-  (every?
-   #(or (= (first %) \_) (= (first %) (second %)))
-   (map list mask word)))
+(def dict-answers (str/split (slurp answers-filename) #"\n"))
+(def dict-allowed-guesses (str/split (slurp allowed-guesses-filename) #"\n"))
 
 
-(java.util.regex.Pattern/compile "\\d")
+;; MASKS
+(def BLACK 0)
+(def YELLOW 1)
+(def GREEN 2)
 
-;; Note could also do guess answer candidate boolean, but this is probably
-;; faster
-(defn matcher-from-words [guess answer]
-  (let [nomatches (apply str (clojure.set/difference (set guess) (set answer)))]
-   nomatches
-  ))
+(def w-word "aback")
+(def seq-chars (seq w-word))
+(def mask-list (list BLACK YELLOW GREEN YELLOW BLACK))
+
+; returns () or (\a \d \d) etc.
+(defn apply-mask-to-word [mask-list seq-word val]
+  (filter identity (map #(if (= val %1) %2) mask-list seq-word)))
+
+;; TODO This is gross and not general but who cares
+(defn generate-all-mask-lists [n]
+  (if (= 1 n) '((0) (1) (2))
+      (let [children (generate-all-mask-lists (dec n))
+            result
+            (reduce concat (list 
+             (map #(conj % BLACK) children)
+             (map #(conj % YELLOW) children)
+             (map #(conj % GREEN) children)))]
+        result)))
+
+(def all-mask-lists (generate-all-mask-lists 5))
+
+(defn _gen-includer-filter [letter]
+  (partial filter #(str/includes? % letter)))
+
+; accepts sequences of chars
+(defn gen-includer-filters [yellow-list]
+  (reduce comp
+          (vec
+           (map _gen-includer-filter
+                (map str yellow-list)))))
+
+(defn _generate-regex-entry [black-list mask-elt word-elt]
+  (cond (= mask-elt GREEN) word-elt
+        (= mask-elt YELLOW) (apply str "[^" black-list word-elt "]")
+        :else   (apply str "[^" black-list "]")))
+
+;; TODO START HERE - not a list?/
+(defn filter-fn-from-mask-list-and-word [w-word mask-list]
+  (let [seq-word (seq w-word)
+        black-list (apply-mask-to-word mask-list seq-word BLACK)
+        yellow-list (apply-mask-to-word mask-list seq-word  YELLOW)
+        green-list (apply-mask-to-word mask-list seq-word GREEN)
+        regex-str (apply str
+                         (map (partial _generate-regex-entry (apply str black-list))
+                              mask-list w-word))
+        filter-regex (partial filter #(re-matches (re-pattern regex-str) %))
+        filter-includes (gen-includer-filters yellow-list)
+        ]
+;        [regex-str yellow-list]
+        (comp filter-regex filter-includes)
+        ))
+        
+
+;  (map #((filter-fn-from-mask-list-and-word w-word %) dict-answers) all-mask-lists)
 
 
 (def fn-d (partial filter #(str/includes? % "D")))
 (def fn-reg1 #(filter (fn [w] (re-matches #"[^ER][^ER]A[^S]E" w)) %))
 
-;;(defn match? 
-;; (filter #(str/includes? % "D") (filter #(re-matches #"[^ER][^ER]A[^S]E" %) dict))
+(defn calculate-entropy [count-dict result-set]
+  (let [c (count result-set)]
+    (cond (= 0 c) 0
+          (= 1 c) (/ (- c) count-dict)
+        :else  (/ (* (Math/log c) c)
+                  (Math/log 2) ; NOTE not strictly necessary
+                  count-dict))))
 
-;; guess: ERASE
-;; word:  SPATE
-;;        0X101
-
-;;   [^E]
-;;   [^ER][^ER]
-;;   [^ER][^ER] A 
-;;   [^ER][^ER] A [^S]
-;;   [^ER][^ER]A[^S]E
-;;   includes S
-
-
-(defn get-filtered-list [s-letters dict]
-  (println "CALLING WITH" s-letters)    
-  (if (= 1 (count s-letters))
-    (filter #(str/includes? % (first s-letters)) dict)
-    (filter #(str/includes? % (first s-letters)) (get-filtered-list (rest s-letters)))))
-  
-(def gfl-memo (memoize get-filtered-list))
-
-;; PLAN:
-
-;; w_guess 
-;; For each word w_guess remaining:
-;;; For each word w_answer in dict:
-;;;; Compute response_mask(w_guess, w_answer)
-;;;; Count non-matches in remaining .04 sec.
-;;;; Return count, eliminated
-;;; Take argmax of count
-;;; remaining -> remaining minus eliminated
-
-(defn regex-entry [s_answer s_eliminated l_guess l_answer]
-  (cond (= l_guess l_answer) l_answer
-        (s_answer l_guess) (str "[^" l_guess s_eliminated "]")
-        :else (str "[^" s_eliminated "]")))
-
-
-; TODO identity vs. memoize  
-; TOOD Assumes dict as global - need to reset if changing at next move
-(defn filter-dict [dict]
-  ( #_memoize identity
-   (fn [set-letters]
-     (print ".")
-     (if (= 1 (count set-letters))
-       (filter #(str/includes? % (first set-letters)) dict)
-       (filter #(str/includes? % (first set-letters))
-               (filter-dict (rest set-letters)))))))
-
-    
-(defn count-remaining [dict w_guess w_answer]
-  (let [s_guess (set w_guess)
-        s_answer (set w_answer)
-        s_eliminated (apply str (clojure.set/difference s_guess s_answer))
-        s_overlap (clojure.set/intersection s_guess s_answer)
-        regex-parts (map (partial regex-entry s_answer s_eliminated)
-                   (map identity w_guess)
-                   (map identity w_answer))
-        regex (re-pattern (apply str regex-parts))
-        dict-overlap (filter-dict (map str s_overlap))
-        result (count (filter #(re-matches regex %) dict-overlap))
-       _ (println w_answer result)
+(defn evaluate-move [all-mask-lists dict-answers w-word]
+  (let [matching-words (map
+                        #((filter-fn-from-mask-list-and-word w-word %) dict-answers)
+                        all-mask-lists)
+        n-entropy (apply +
+                      (map (partial calculate-entropy (count dict-answers))
+                       matching-words))
         ]
-        result
-       ))
-  
+    (list n-entropy matching-words)))
 
+(defn evaluate-all-moves [dict-answers dict-allowed-guesses]
+  (let [all-mask-lists (generate-all-mask-lists 5)
+        results (map list
+                 dict-allowed-guesses
+                 (map (partial evaluate-move all-mask-lists dict-answers)
+                      dict-allowed-guesses))
+        sorted-results (sort #(< (first (second %1)) (first (second %2))) results)
+        ]
+;    results
+    sorted-results
+    ))
 
-;; TODO : Pre-generate all filtered sets of letters
-(defn eliminators [w_guess dict]
-  (zipmap dict (map (partial count-remaining dict w_guess) dict)))
-
-; (defn 
-
-;   (eliminators "STEAM")
-;   (count-remaining dict "STEAM" "BEVOR")
-
-  
-
-;; eliminated-all(dict, remaining, word)
-;;   = sum(eliminated_count(dict, word_i)) for all word_i in dict
-
-;; because each word has equal probability, 
+(defn just-words-and-entropy [evaluations]
+  (map #(list (first %) (first (second %))) evaluations))
