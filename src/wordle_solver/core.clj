aeafc2aa8bfe3ae95ac99b1acaa3ed6265b9e4b8
Dave Fetterman <fettermania@gmail.com>
2022-03-03 12:09:59 -0800

    More scratch work, new dictionary for NYT

--- src/wordle_solver/core.clj
+++ src/wordle_solver/core.clj
@@ -160,22 +160,32 @@
 #_(do
 (def l-answers dict-answers)
 (def l-allowed-guesses dict-allowed-guesses)
+(def r-top   (evaluate-all-moves l-answers l-allowed-guesses))
+(def r-evals r-top)
 )
 
 #_(do
+    
+    ;; NOTE: In Hard mode, l-allowed-guesses are CLOSE to l-answers
+    ;; There are also "black-disqualified answers" that
+    ;; are still legal moves
+;    (def l-allowed-guesses l-answers)
   (evaluate-all-moves l-answers l-allowed-guesses)
   (def r-evals *1)
   (pprint (take 10 (just-words-and-entropy r-evals)))
 
+  (def viable-answer-words  (filter #(get (set l-answers) (first %))
+                                    (just-words-and-entropy r-evals)))
+  (pprint (take 10 viable-answer-words))
   
-  (def w-word "outed")
-  (def response-mask '(1 0 0 1 0))
-
+  (def w-word "court")
+  (def response-mask '(0 2 2 2 0))
   (play-move l-answers l-allowed-guesses r-evals w-word response-mask)
   (def l-answers *1)
   
 )
 
+;; (pprint (take 10 (just-words-and-entropy r-evals)))
 ;;(pprint (extract-row-from-results r-evals "cairn"))
 ; (clojure.pprint/pprint *map* (clojure.java.io/writer "foo.txt"))
 
@@ -186,3 +196,84 @@
 ;; (correct one) -> 0 2 0 1 0 = [^DCA]A[^DCA][^HDCA][^DCA] includes H
 ;; but hairy will show up in both sets as "matching" 
 
+;; TODO Another bug - an answer set with one option makes everything -1 (b/c you know the answer!)
+
+;; TODO Evaluate all moves - include "
+
+;; TODO Did the word list change to exclude SORAE on 1/27?
+
+;; TODO Filter results to include hard-mode-acceptable words
+;;  (pprint (take 20
+;;  (filter #(-> % first seq second (= \o))
+;; (just-words-and-entropy r-evals))))
+;; (filter #(get (set l-answers) (first %)) (just-words-and-entropy r-evals))
+
+
+(defn drop-nth-from-seq [n seq-w]
+  (concat
+    (take n seq-w)
+    (drop (inc n) seq-w)))
+
+#_(do 
+(def viable-answers (filter #(-> % first seq second (= \o))
+                            (filter #(get (set l-answers) (first %))
+                                    (just-words-and-entropy r-evals))))
+
+(def viable-guesses-all (filter #(-> % first seq second (= \o))
+                                    (just-words-and-entropy r-evals)))
+
+(def viable-answer-words  (filter #(get (set l-answers) (first %))
+                                    (just-words-and-entropy r-evals)))
+
+
+                                        ; What words have five or more common endings?
+
+;; TODO
+(defn select-from-word [indices w]
+  (let [s (seq w)]
+  (map #(nth s %) indices)))
+
+#_(do
+(def endings-map (group-by rest dict-answers))
+(def starters-map (group-by (partial take 4) dict-answers))
+
+(def dict-answers  (into '()  (clojure.set/intersection (set three-or-more-ender-words) (set two-or-more-starter-words))))
+)
+
+(def block-24
+(mapcat
+ second
+ (filter
+  (fn [[k v]] (> (count v) 3))
+  (group-by (partial select-from-word '(2 4)) dict-answers))))
+
+(def block-014
+(mapcat
+ second
+ (filter
+  (fn [[k v]] (> (count v) 1))
+  (group-by (partial select-from-word '(0 1 4)) dict-answers))))
+
+(def block-124
+(mapcat
+ second
+ (filter
+  (fn [[k v]] (> (count v) 1))
+  (group-by (partial select-from-word '(1 2 4)) dict-answers))))
+
+(def l-answers-2-24 (into '() (clojure.set/intersection (set block-012) (set block-014) (set block-124))))
+
+    
+#_(do
+    (def gbggg-words
+      (mapcat second
+(filter (fn [[k v]] (> (count v) 2)) (group-by #(drop-nth-from-seq 1 (seq %)) dict-answers)))
+   ))
+  (def l-answers gbggg-words)
+)
+
+;; ("skill" "spill" "swill" "silly" "sully")
+;;(zipmap (map first viable-answer-words)
+  ;;      (map (partial extract-row-from-results r-evals) (map first viable-answer-words)))
+
+;; Another TODO - serialize and unserliaze (perhaps removing empties?
