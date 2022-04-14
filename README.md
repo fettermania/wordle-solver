6883c1bb7985d6a8860268e0a7c583405297b9e7
Dave Fetterman <fettermania@gmail.com>
2022-04-14 12:47:11 -0700

    Progress on fixing the DACHA filter problem

--- README.md
+++ README.md
@@ -79,11 +79,60 @@ Entropy is used in a similar way to Alex Healy's treatment here: http://www.alex
 
 #### Major: Answers duplicated across response buckets.
 
-Example: DACHA vs. HAIRY
+Example: DACHA vs. correct answer HAIRY
 ;0 2 0 1 1 = [^DC]A[^DC][^HDC] includes A, H
 (correct one) -> 0 2 0 1 0 = [^DCA]A[^DCA][^HDCA][^DCA] includes H
 but hairy will show up in both sets as "matching" 
 
+Example: Guess ASSES vs. answer MASSE SHOULD yield 1 1 2 1 0, but it NOW yields 1 1 2 1 1
+1: Y - There's an A, and it's not here.
+2: Y- There's a not green S somewhere else in the puzzle
+3: G- This is an actual match
+4: Y - There's a not green E somewhere else in the puzzle
+5: B - There's no S in this spot, and there is no spot for this S.
+
+We can state: The number of S's in the answer is the number of greens plus yellows.  
+So the rule is: Grep for (has exactly Y+G S's)
+
+ASSES could also be scored against answer MASSE in two ways, then.
+10211 - the last S gets credit
+11210 - the first S gets credit - correct answer
+
+The yellows will distribute left-to-right, so the alphabetically LAST answer is the correct one for any duplicates.
+
+
+What is the actual rule?
+- A square is green if that slot has that letter.
+- A square is yellow if, removing all greens, that 
+
+====
+BUG 2: If the answer is PASSE, then DACHA should be 0 2 0 0 0, not that AND 0 2 0 0 1 (count of A yellows+greens should be 1, not 2)
+Bug 3: If the answer is BLAND, then DACHA should be 1 1 0 0 0, not 1 1 0 0 1 (by count)
+Bug 3a: TODO why isn't it generating 1 0 0 0 1 as well? 
+TODO Look at the eval function strings for each, include in r-evals?
+
+START AT: NOTE 4-8-regexes and 4-8-tree
+
+FOR EACH LETTER, createa  (G, Y, G)  (B), (Y B) (B Y)... list when seen.
+
+FIX 1: If spot is green, put letter in that spot in regex, add +1 to total count for that letter.
+FIX 2: If spot is yellow, filter letter out of that spot in regex, add + 1 to the total count needed for that letter.
+FIX 3: If spot is black, filter letter out of that spot in the regex.   If there are no previous [or, by rule 4, any] yellow spots with that letter for it, throw in regex as exclude from every space.  Otherwise, can't.
+FIX 4: If spot is yellow and there's a previous black for that letter, return totally empty filter.
+RESULT: 
+- Get empty flag, regex, letter -> count
+- If empty, return negative
+- Else generate letter filters, regex function, and compose all of these (order doesn't matter)
+
+TODO NEXT: This is the path:
+- Compute regex for all: green is in, yellow or black is out for that space.  That's the regex
+- Compute total count for all.  If there's a black, it's equal.  If not, it's at least that many.  Those are the other functions.
+- Ensure no black precedes a yellow.  If so, make a null.
+- first step : get to
+{\a: (BLACK YELLOW) \b: (GREEN) }etc.
+ (update (update {} \a conj 1) \a conj 2)
+
+EXTRA FIX: remove the kludge / kluge throw in earlier.
 
 #### Enhancement: Build hard mode in
 
--- src/wordle_solver/core.clj
+++ src/wordle_solver/core.clj
@@ -13,19 +13,28 @@
 (def dict-answers (str/split (slurp answers-filename) #"\n"))
 (def dict-allowed-guesses (str/split (slurp allowed-guesses-filename) #"\n"))
 
+(def test-answers-filename "test-wordle-answers.txt")
+(def test-allowed-guesses-filename "test-wordle-allowed-guesses.txt")
+
+(def test-dict-answers (str/split (slurp test-answers-filename) #"\n"))
+(def test-dict-allowed-guesses (str/split (slurp test-allowed-guesses-filename) #"\n"))
+
 
 ;; MASKS
 (def BLACK 0)
 (def YELLOW 1)
 (def GREEN 2)
 
-(def mask-list (list BLACK YELLOW GREEN YELLOW BLACK))
+; DEMO CODE 
+;(def mask-list (list BLACK YELLOW GREEN YELLOW BLACK))
 
 ; returns () or (\a \d \d) etc.
 (defn apply-mask-to-word [mask-list seq-word val]
   (filter identity (map #(if (= val %1) %2) mask-list seq-word)))
 
-;; TODO This is gross and not general but who cares
+;; NOTE This is gross and not general but who cares
+;; returns list(mask)
+;; example return: ((0 0 0 0 0) (0 0 0 0 1) (0 0 0 0 2) ... (2 2 2 2 1) (2 2 2 2 2))
 (defn generate-all-mask-lists [n]
   (if (= 1 n) '((0) (1) (2))
       (let [children (generate-all-mask-lists (dec n))
@@ -36,11 +45,16 @@
              (map #(conj % GREEN) children)))]
         result)))
 
+;; GLOBAL - all 3^5 mask lists
 (def all-mask-lists (generate-all-mask-lists 5))
 
+
+;; takes "a" and returns function that strips out everything but (\a \a \a...)
+;; example: ((_gen-includer-filter "a") "adage"
 (defn _gen-includer-filter [letter]
   (partial filter #(str/includes? % letter)))
 
+; note: needs green list too - TODO: match EXACTLY this count
 ; accepts sequences of chars
 (defn gen-includer-filters [yellow-list]
   (reduce comp
@@ -61,13 +75,87 @@
         regex-str (apply str
                          (map (partial _generate-regex-entry (apply str black-list))
                               mask-list w-word))
+        _ (println "Regex str for " w-word ", mask " (str mask-list) ": " regex-str)
         filter-regex (partial filter #(re-matches (re-pattern regex-str) %))
         filter-includes (gen-includer-filters yellow-list)
         ]
         (comp filter-regex filter-includes)
         ))
-        
 
+;; TODO NEW ZONE  
+ ; (defn new-filter-fn-from-mask-list-and-word [w-word mask-list]
+ ;   (map-indexed (fn [idx item] [item idx]) (map list w-word mask-list)  
+ ; ))
+
+
+;  (color-seqs-from-word-and-mask "abase" '(0 1 2 1 0))
+;   yields {\a (2 0), \b (1), \s (1), \e (0)}
+; Note that sequences are reversed from the order in word
+(defn rev-color-seqs-from-word-and-mask [w-word mask-list]
+  (loop [word w-word 
+  			    mask mask-list
+  						accum {}]
+  						(if (empty? word)  ;; note: assuming |word| = |mask-list|
+  							accum
+  							(recur 
+  							  	 	(rest word)
+ 									 	 	(rest mask)
+	  	 									(update accum (first word) conj (first mask))))))
+
+;; Note: This might be prettier as "0.*1" in a regex
+(defn rev-seq-contains-black-then-yellow [rev-color-seq]
+  (let [rev-color-str (apply str rev-color-seq)
+  					 color-str (apply str (reverse rev-color-seq))
+  					 first-black-ix (.indexOf color-str "0")
+  					 last-yellow-reverse-ix (.indexOf  rev-color-str "1")
+  					 last-yellow-ix (- (dec (count rev-color-seq))
+  					 										last-yellow-reverse-ix)]
+  					(and (not= first-black-ix -1)
+  									 (not= last-yellow-reverse-ix -1)
+  									 (< first-black-ix last-yellow-ix))))
+
+  									
+;; TODO 4/14 - look at these as filters?  
+
+;; (def two-or-more-as (gen-char-count-fn \a 2 false))
+;; (two-or-more-as "abadc") --> true
+;; (two-or-more-as "abddc") --> false
+
+;; (def two-as (gen-char-count-fn \a 2 true))
+;; (two-as "abcda") --> true
+;; (two-as "abada") --> false
+(defn gen-char-count-fn [ch-match ct-expected eq?]
+   (let [comp-fn (if eq? = >=)]
+    (fn [w] 
+     (comp-fn 
+     		(count (filter (partial = ch-match) w))
+    			ct-expected))))
+
+(defn regex-from-word-and-mask [w-word mask-list]
+   (apply str
+     (map 
+       (fn [w m]
+         (if (= m GREEN) w (str "[^" w "]")))
+   		w-word
+   		mask-list)))
+
+;; \a (2 0)
+(defn rev-color-seq-to-filter-fn [ch rev-color-seq]
+  ;; if contains 1 0 (black "before" yellow), it's always fanlse
+  ;; if it contains a 0, then it's exact count of non-0s
+  ;; otherwise its that many non-0s or more
+  (let [ct-black (count (filter (partial = 0) rev-color-seq))
+  						ct-yg (count (filter (partial not= 0) rev-color-seq))
+  						has-black-then-yellow 
+  							(rev-seq-contains-black-then-yellow rev-color-seq)
+  							]
+  	(if has-black-then-yellow 
+  	    (fn [w] false)
+  				 (gen-char-count-fn ch ct-yg (> ct-black 0)))))
+
+
+
+;; TODO END NEW ZONE
 
 (def fn-d (partial filter #(str/includes? % "D")))
 (def fn-reg1 #(filter (fn [w] (re-matches #"[^ER][^ER]A[^S]E" w)) %))
@@ -82,7 +170,7 @@
   (let [matching-words (map
                         #((filter-fn-from-mask-list-and-word w-word %)
                           dict-answers) all-mask-lists)
-        ;; Klugey fix here.  Total words should always be dict-answers
+        ;; TODO Klugey fix here.  Total words should always be dict-answers
         ;; but some patterns overlap so words are doubled.
         ;; This at least fixes the denominator
         total-words (apply + (map count matching-words))
@@ -157,6 +245,17 @@
      (fn [[k v]] (>= (count v) 2))
        (group-by (comp seq sort)
                  dict-answers))))
+
+
+; NOTE - there's probably a better way to clean out empties from the visual print
+(defn clean-results-row [r-row]
+  (let [mymap (:matches (second r-row))]
+    [(first r-row)
+      {
+        :entropy (:entropy (second r-row))
+        :matches (select-keys mymap (for [[k v] mymap :when (not (empty? v))] k))
+      }
+    ]))
 
 ;; USAGE
 
