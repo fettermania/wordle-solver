aa16f59380e6dd4a2b931ccef00a16bd3c61d87f
christopherlaangsell <56093917+christopherlaangsell@users.noreply.github.com>
2024-01-06 10:44:19 -0600

    Add puget dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [mvxcvi/puget "1.3.4"]]
   :main ^:skip-aot wordle-solver.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
